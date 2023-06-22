dee8bed8483d5e7799be5499d3cd9201bc9a4f66
Nguyen Van Thong <vanthong07012002@gmail.com>
2023-06-22 22:41:19 +0700

    comment test

--- src/test/java/com/gangoffive/shippingfast/ShippingFast/ShippingFastApplicationTests.java
+++ src/test/java/com/gangoffive/shippingfast/ShippingFast/ShippingFastApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ShippingFastApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
